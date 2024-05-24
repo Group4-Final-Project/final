e36cb7cb20f3b8eabc5901cf0ff4f11617d0ba2b
dasberaht <v.wwhee@gmail.com>
2024-05-24 11:14:15 +0900

    prototype

---
+++ www/www/src/main/java/com/ezen/www/service/MemberServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ezen.www.service;
+
+import com.ezen.www.repository.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberMapper memberMapper;
+
+}
--- www/www/src/test/java/com/ezen/www/BootMybatis2ApplicationTests.java
+++ www/www/src/test/java/com/ezen/www/BootMybatis2ApplicationTests.java
@@ -1,7 +1,7 @@
 package com.ezen.www;
 
-import com.ezen.www.domain.BoardVO;
-import com.ezen.www.repository.BoardMapper;
+//import com.ezen.www.domain.BoardVO;
+//import com.ezen.www.repository.BoardMapper;
 import org.junit.jupiter.api.Test;
 //import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,21 +12,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class BootMybatis2ApplicationTests {
 
-	@Autowired
-	private BoardMapper mapper;
-
-	@Test
-	void contextLoads() {
-		for(int i=0; i<300; i++){
-			BoardVO bvo = BoardVO
-					.builder()
-					.title("Title "+i)
-					.writer("tester@test.com")
-					.content("Test Content")
-					.build();
-
-			mapper.insert(bvo);
-		}
-	}
+//	@Autowired
+//	private BoardMapper mapper;
+//
+//	@Test
+//	void contextLoads() {
+//		for(int i=0; i<300; i++){
+//			BoardVO bvo = BoardVO
+//					.builder()
+//					.title("Title "+i)
+//					.writer("tester@test.com")
+//					.content("Test Content")
+//					.build();
+//
+//			mapper.insert(bvo);
+//		}
+//	}
 
 }
