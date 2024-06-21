0857c41ed8bd1c7d8018cfe3b4cb01652860fd84
EZENIC-170 <apfhdfhrrl@naver.com>
2024-06-21 18:22:12 +0900

    2024-06-21

--- www/www/src/main/resources/static/dist/js/select-options.js
+++ www/www/src/main/resources/static/dist/js/select-options.js
@@ -2,55 +2,157 @@ document.getElementById('size').addEventListener('change', updateResult);
 document.getElementById('fur').addEventListener('change', updateResult);
 document.getElementById('aggression').addEventListener('change', updateResult);
 document.getElementById('temporary').addEventListener('change', updateResult);
+document.getElementById('CutM').addEventListener('change', updateResult);
 
-     // 이미지 URL을 저장하는 객체
-    const imageUrls = {
-        1: '/dist/image/cut.jpg',
-        2: '/dist/image/petBath.jpg',
-        3: '/dist/image/pethygieneCare.jpg',
-        4: '/dist/image/petCare.jpg',
-        5: '/dist/image/ticket-100.jpg',
-        6: '/dist/image/ticket-50.jpg',
-        7: '/dist/image/ticket-30.jpg',
-        8: '/dist/image/petFilming.png',
-    };
 
-    // idx 값에 따라 이미지를 변경하는 함수
-    function changeImage(idx) {
-        const imageUrl = imageUrls[idx];
-        if (imageUrl) {
-            const imgElement = document.querySelector('.item-img');
-            imgElement.src = imageUrl;
+function updateResult(event) {
+    const idx = getQueryParam('idx');
+    const sizeVal = document.getElementById('size').value;
+    const furVal = document.getElementById('fur').value;
+    const aggressionVal = document.getElementById('aggression').value;
+    const CutMVal = document.getElementById('CutM').value;
+    const temporaryVal = document.getElementById('temporary').value;
+
+    const fullTotal = document.getElementById('full-total');
+    const select = event.target;
+    const label = select.options[0].textContent;
+    const selected = select.selectedOptions[0].textContent;
+    const value = select.value;
+
+    let price = 0;
+
+    // 기본사항에서의 가격 계산
+    if (sizeVal !== '크기 (필수)') {
+        switch (idx) {
+            case '1':
+                switch (sizeVal) {
+                    case '1':
+                        price += 50000;
+                        fullTotal.textContent += price;
+                        break;
+                    case '2':
+                        price += 60000;
+                        fullTotal.textContent += price;
+                        break;
+                    case '3':
+                        price += 70000;
+                        fullTotal.textContent += price;
+                        break;
+                }
+                break;
+            case '2':
+                switch (sizeVal) {
+                    case '1':
+                        price += 52000;
+                        fullTotal.textContent += price;
+                        break;
+                    case '2':
+                        price += 62000;
+                        fullTotal.textContent += price;
+                        break;
+                    case '3':
+                        price += 72000;
+                        fullTotal.textContent += price;
+                        break;
+                }
+                break;
         }
     }
 
-    function updateResult(event) {
-        const select = event.target;
-        const label = select.options[0].textContent;
-        const selected = select.selectedOptions[0].textContent;
-        const value = select.value;
-
-        // Update result div with the new selection
-        const resultDiv = document.getElementById('result');
-        const existingBox = document.querySelector(`#result .${select.id}-box`);
-
-
-
-        if (value !== label) {
-            if (existingBox) {
-                existingBox.querySelector('p').textContent = `${label}: ${selected}: ${value}원`;
-            } else {
-                resultDiv.appendChild(createResultBox(label, value, selected, select.id));
-            }
+    // 추가사항에서의 가격 계산
+    if (furVal !== '털엉킴,뭉침 추가 (선택)') {
+        switch (furVal) {
+            case '1':
+                price += 5000;
+                fullTotal.textContent += price;
+                break;
+            case '2':
+                price += 10000;
+                fullTotal.textContent += price;
+                break;
+            case '3':
+                price += 15000;
+                fullTotal.textContent += price;
+                break;
         }
-
-        // Reset the select box to the default option
-        select.value = label;
     }
 
+    if (aggressionVal !== '공격성 추가 (선택)') {
+        switch (aggressionVal) {
+            case '1':
+                price += 5000;
+                fullTotal.textContent += price;
+                break;
+            case '2':
+                price += 10000;
+                fullTotal.textContent += price;
+                break;
+            case '3':
+                price += 15000;
+                fullTotal.textContent += price;
+                break;
+        }
+    }
+
+    if (CutMVal !== '미리수 (선택)') {
+        switch (CutMVal) {
+            case '1':
+                price += 5000;
+                fullTotal.textContent += price;
+                break;
+            case '2':
+                price += 10000;
+                fullTotal.textContent += price;
+                break;
+        }
+    }
+
+    if (temporaryVal !== '임시 (선택)') {
+        switch (temporaryVal) {
+            case '1':
+                price += 2000;
+                fullTotal.textContent += price;
+                break;
+            case '2':
+                price += 2000;
+                fullTotal.textContent += price;
+                break;
+            case '3':
+                price += 1000;
+                fullTotal.textContent += price;
+                break;
+            case '4':
+                price += 1000;
+                fullTotal.textContent += price;
+                break;
+        }
+    }
+
+    // Update result div with the new selection
+    const resultDiv = document.getElementById('result');
+    
+    const existingBox = document.querySelector(`#result .${select.id}-box`);
+    const existingPrice = existingBox ? existingBox.querySelector('.PriceTotal') : null;
+
+    if (value !== label) {
+        if (existingBox && existingPrice) {
+            existingBox.querySelector('p').textContent = `${label}: ${selected}`;
+            existingPrice.textContent = `${price}원`;
+        } else {
+            resultDiv.appendChild(createResultBox(label, value, selected, select.id, price));
+        }
+    }
+
+    // Update full total
+    fullTotal.textContent = `총액: ${price}원`;
+
+    // Reset the select box to the default option
+    select.value = label;
+}
 
 
-    function createResultBox(label, value, selected, selectId) {
+
+    function createResultBox(label, value, selected, selectId, price) {
         const box = document.createElement('div');
         box.classList.add(`${selectId}-box`);
         box.style.border = '1px solid #ccc';
@@ -66,7 +168,7 @@ document.getElementById('temporary').addEventListener('change', updateResult);
     
         // 텍스트 표시용 span 요소 생성
         const text = document.createElement('p');
-        text.textContent = `${label}: ${selected}: ${value}원`;
+        text.textContent = `${label}: ${selected}`;
         // text 요소를 contentWrapper에 추가합니다.
         contentWrapper.appendChild(text);
     
@@ -86,41 +188,81 @@ document.getElementById('temporary').addEventListener('change', updateResult);
     
         // contentWrapper를 box에 추가합니다.
         box.appendChild(contentWrapper);
-    
+        
+        const hr = document.createElement('hr');
+        box.appendChild(hr);
+
+        const bottombox = document.createElement('div');
+        bottombox.id='bottombox';
+        const PriceTotal = document.createElement('span');
+        PriceTotal.classList.add('PriceTotal');
+        PriceTotal.style.fontSize='18px';
+        PriceTotal.textContent=`${price}원`;
+
+        bottombox.appendChild(PriceTotal);
+        box.appendChild(bottombox);
+
     
         return box;
     }
 
-    // 쿼리스트링 값을 특정 이름의 파라미터로 받기
-    function getQueryParam(param) {
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get(param);
-    }
-
+// 쿼리스트링 값을 특정 이름의 파라미터로 받기
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
 
 // idx 쿼리 파라미터에 따라 h3의 내용과 temp의 html을 바꾸기
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
     const idx = getQueryParam('idx');
-    const idxHtml = document.getElementById('temporary');
+    const fur = document.getElementById('fur');
+    const aggression = document.getElementById('aggression');
+    const temporary = document.getElementById('temporary');
+    const CutM = document.getElementById('CutM');
 
-    if (idx === '1') {
-        idxHtml.innerHTML = `
-            <option value="부분스타일 (선택)" selected>부분스타일 (선택)</option>
-            <option value="4">얼굴</option>
-            <option value="5">귀</option>
-            <option value="6">발</option>
-            <option value="7">꼬리</option>
+    if(idx === '1'){
+        fur.style.display="block";
+        aggression.style.display="block";
+        temporary.style.display="block";
+        CutM.style.display="block";
+
+        fur.innerHTML=`
+        <option value="털엉킴,뭉침 추가 (선택)" selected>털엉킴,뭉침 추가 (선택)</option>
+        <option value="1">1kg-9.9kg</option>
+        <option value="2">10kg-14.9kg</option>
+        <option value="3">15kg-30kg</option>
         `;
-    } else if (idx === '2') {
-        idxHtml.innerHTML = `
-            <option value="머드팩 (선택)" selected>머드팩 (선택)</option>
-            <option value="1">소형</option>
-            <option value="2">중형</option>
-            <option value="3">대형</option>
+
+        aggression.innerHTML=`
+        <option value="공격성 추가 (선택)" selected>공격성 추가 (선택)</option>
+        <option value="1">1kg-9.9kg</option>
+        <option value="2">10kg-14.9kg</option>
+        <option value="3">15kg-30kg</option>
         `;
+
+        CutM.innerHTML=`
+        <option value="미리수 (선택)" selected>미리수 (선택)</option>
+        <option value="1">6mm</option>
+        <option value="2">1cm</option>
+        `;
+
+        temporary.innerHTML=`
+        <option value="추가부분 (선택)" selected>추가부분 (선택)</option>
+        <option value="1">얼굴</option>
+        <option value="2">귀</option>
+        <option value="3">발</option>
+        <option value="4">꼬리</option>
+        `;
+
     }
-}
-function setSelectOption() {
+
+
+});
+
+
+
+
+function setSelectSizeOption() {
     const idx = getQueryParam('idx');
     const size = document.getElementById('size');
 
@@ -131,21 +273,22 @@ function setSelectOption() {
     if (idx === '1') {
         setInnerHTML(`
             <option value="크기 (필수)" selected>크기 (필수)</option>
-            <option value="50000">소형</option>
-            <option value="60000">중형</option>
-            <option value="70000">대형</option>
+            <option value="1">소형</option>
+            <option value="2">중형</option>
+            <option value="3">대형</option>
         `);
     }else if(idx === '2'){
         setInnerHTML(`
             <option value="크기 (필수)" selected>크기 (필수)</option>
-            <option value="52000">소형</option>
-            <option value="62000">중형</option>
-            <option value="72000">대형</option>
+            <option value="1">소형</option>
+            <option value="2">중형</option>
+            <option value="3">대형</option>
         `);
     }
 }
 
-setSelectOption();
+setSelectSizeOption();
+
 
 
 function setIdxDiplay(){
@@ -192,8 +335,29 @@ function setOptionTitle(){
     }
 }
 setOptionTitle();
-// 상세 내용 이미지
 
+ // 이미지 URL을 저장하는 객체
+ const imageUrls = {
+    1: '/dist/image/cut.jpg',
+    2: '/dist/image/petBath.jpg',
+    3: '/dist/image/pethygieneCare.jpg',
+    4: '/dist/image/petCare.jpg',
+    5: '/dist/image/ticket-100.jpg',
+    6: '/dist/image/ticket-50.jpg',
+    7: '/dist/image/ticket-30.jpg',
+    8: '/dist/image/petFilming.png',
+};
+
+// idx 값에 따라 이미지를 변경하는 함수
+function changeImage(idx) {
+    const imageUrl = imageUrls[idx];
+    if (imageUrl) {
+        const imgElement = document.querySelector('.item-img');
+        imgElement.src = imageUrl;
+    }
+}
+
+// 상세 내용 이미지
 const DetailimageUrls = {
     1: '/dist/image/CareService-cut.webp', // 800X 5000px
     2: '/dist/image/CareService-bath.webp', // 800X 4000px
@@ -272,4 +436,5 @@ document.addEventListener('DOMContentLoaded', function() {
     setDetailImageSize(idx);
     DetailchangeImage(idx);
     changeImage(idx);
+    
 });
